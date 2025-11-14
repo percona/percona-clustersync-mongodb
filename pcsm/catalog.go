9969de7be14f6c5cb47d819ecb9c449f5733c904
Inel Pandzic <inel.pandzic@percona.com>
2025-11-14 19:24:27 +0100

    Add enforceUniquenessCheck option to shardCollection command.

--- pcsm/catalog.go
+++ pcsm/catalog.go
@@ -1160,13 +1160,15 @@ func (c *Catalog) ShardCollection(
 	unique bool,
 ) error {
 	cmd := bson.D{
-		{"shardCollection", db + "." + coll},
-		{"key", shardKey},
-		{"collation", bson.D{{"locale", "simple"}}},
+		{Key: "shardCollection", Value: db + "." + coll},
+		{Key: "key", Value: shardKey},
 	}
 
 	if unique {
-		cmd = append(cmd, bson.E{"unique", true})
+		cmd = append(cmd,
+			bson.E{Key: "unique", Value: true},
+			bson.E{Key: "enforceUniquenessCheck", Value: false},
+		)
 	}
 
 	err := runWithRetry(ctx, func(ctx context.Context) error {
