fa2e59263dfd5609d7ba07bd646a25893334ee52
Adnan <chupe@chupe.ba>
2026-08-20 03:52:22 +0200

    PCSM-322: Fix catalog constructor in clone integration test

--- pcsm/clone/clone_integration_test.go
+++ pcsm/clone/clone_integration_test.go
@@ -256,7 +256,7 @@ func TestClone_DoesNotLoseInflightWrite(t *testing.T) {
 	sourceVer, err := mdb.Version(ctx, source)
 	require.NoError(t, err, "source server version")
 
-	cat := catalog.NewCatalog(target, sourceVer)
+	cat := catalog.NewCatalog(source, target, sourceVer)
 	nsFilter := sel.MakeFilter([]string{dbName + ".*"}, nil)
 	c := clone.NewClone(source, target, cat, nsFilter, &clone.Options{})
 
